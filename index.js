40dcfe6d8da39ac7e4e4c16d904afe83af226db4
chhabiraniojha <chhabiraniojha@gmail.com>
2024-06-17 02:01:38 +0530

    proxy added

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bodyParser=require("body-parser")
 const cors=require("cors")
 require('dotenv').config();
 const paymentRoute=require("./routes/payment routes/payment")
+const httpProxy = require('http-proxy');
+const apiProxy = httpProxy.createProxyServer();
 
 
 
@@ -10,6 +12,14 @@ const app=express();
 
 app.use(bodyParser.json({extended:false}));
 app.use(cors())
+app.use((req, res) => {
+    apiProxy.web(req, res, { target: 'http://172.212.91.177:3000' }, (error) => {
+        if (error) {
+            console.error('Proxy error:', error);
+            res.status(500).send('Proxy error');
+        }
+    });
+});
 
 // app.use("/",(req,res)=>{
 //     res.send("hello")
